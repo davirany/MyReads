68e9cc5d7cbbe50665fd527758223d32dcf3c631
davi_rany <davirany@gmail.com>
2022-04-17 21:07:39 -0300

    Create SearchPage Component

---
+++ src/Components/SearchPage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default class SearchPage extends React.Component {
+    render(){
+        return(
+            <div className="search-books">
+            <div className="search-books-bar">
+              <button className="close-search" onClick={()=>{}}>Close</button>
+              <div className="search-books-input-wrapper">
+                  <input type="text" placeholder="Search by title or author"/>
+              </div>
+            </div>
+            <div className="search-books-results">
+              <ol className="books-grid"></ol>
+            </div>
+          </div>
+        )
+    }
+}
