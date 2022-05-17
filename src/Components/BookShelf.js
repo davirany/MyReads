a520f883d87cb8c3de0c25a2889d3e9b2866e000
davi_rany <davirany@gmail.com>
2022-05-17 03:34:39 -0300

    Remove bug on BookShelf Component

--- src/Components/BookShelf.js
+++ src/Components/BookShelf.js
@@ -2,13 +2,11 @@ import React from "react";
 
 export default function BookShelf(props) {
   return (
-    camalized.length > 0 && (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{props.shelfTitle}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">{props.children}</ol>
-        </div>
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{props.shelfTitle}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">{props.children}</ol>
       </div>
-    )
+    </div>
   );
 }
